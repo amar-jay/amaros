b730c1454167d13ba7dbbe4b9b80200fe6d14be2
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2026-03-19 23:02:37 +0300

    feat(cron): enhance cron job parser for improved scheduling flexibility

--- examples/cron/main.go
+++ examples/cron/main.go
@@ -70,7 +70,13 @@ func main() {
 		},
 	})
 
-	c := cron.New(cron.WithSeconds())
+	c := cron.New(
+		cron.WithParser(
+			cron.NewParser(
+				cron.SecondOptional | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor,
+			),
+		),
+	)
 	c.Start()
 	defer c.Stop()
 
